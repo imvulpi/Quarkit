9649780a0b44fb3b816ca8f60b72196422b0109a
Vulpi <78976654+imvulpi@users.noreply.github.com>
2026-05-16 20:32:54 +0200

    feat: add brieflz extractor logic.

---
+++ runtimes/windows/modules/brieflz/brieflz-extractor.c
@@ -0,0 +1,114 @@
+#define MODULE_EXTRACTION
+#ifdef MODULE_EXTRACTION
+#include <windows.h>
+
+#ifdef MODULE_LOGGING
+extern void print(const char* msg);
+#endif
+
+/* --- BriefLZ start --- */
+/* From BriefLZ - see: deps/brieflz */
+
+#if CHAR_BIT == 8
+/// @brief Gets the low-order 8 bits of a value. 
+#  define octet(v) ((unsigned char) (v))
+#else
+/// @brief Gets the low-order 8 bits of a value. 
+#  define octet(v) ((v) & 0x00FF)
+#endif
+
+/// @brief Size of the header = 24.
+#define HEADER_SIZE (6 * 4)
+
+/// @brief Default block size = 1MB.
+#define DEFAULT_BLOCK_SIZE (1024 * 1024UL)
+
+/// @brief Decompresses `depacked_size` bytes of data from `src` to `dst`
+/// @param src pointer to compressed data
+/// @param dst pointer to where to place decompressed data
+/// @param depacked_size size of decompressed data
+/// @return size of decompressed data
+extern unsigned long blz_depack(const void *src, void *dst, unsigned long depacked_size);
+
+/// @brief Get max block size of compressed data.
+/// @param src_size Default block size.
+size_t blz_max_block_size(size_t src_size)
+{
+	return src_size + src_size / 8 + 64;
+}
+
+/// @brief Read a 32-bit unsigned value in network order.
+static unsigned long
+read_be32(const unsigned char *p)
+{
+	return ((unsigned long) octet(p[0]) << 24)
+	     | ((unsigned long) octet(p[1]) << 16)
+	     | ((unsigned long) octet(p[2]) << 8)
+	     | ((unsigned long) octet(p[3]));
+}
+
+/* --- BriefLZ end --- */
+
+/// @brief Pointer to the start of the binary payload.
+extern unsigned char _payload_start[];
+
+/// @brief Pointer to the end of the binary payload.
+extern unsigned char _payload_end[];
+
+/// @brief Extracts and decompresses the data to the specified file.
+/// @param filename Path to the file.
+void extract(const char* filename){
+    size_t src_len = (_payload_end - _payload_start);
+    unsigned long blocksize = DEFAULT_BLOCK_SIZE;
+	size_t max_block_size = blz_max_block_size(blocksize);
+
+    HANDLE handle = GetProcessHeap();
+    HANDLE hFile = CreateFileA(filename, FILE_APPEND_DATA, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    unsigned char* data = (unsigned char *) HeapAlloc(handle, 0, blocksize);
+    unsigned char* packed = (unsigned char *) HeapAlloc(handle, 0, max_block_size);
+
+    unsigned char* currentPtr = _payload_start;
+    size_t depackedsize = 0;
+    while(currentPtr < _payload_end - HEADER_SIZE){
+        size_t hdr_packedsize, hdr_depackedsize;
+
+        hdr_packedsize = (size_t) read_be32(currentPtr + 2 * 4);
+        hdr_depackedsize = (size_t) read_be32(currentPtr + 4 * 4);
+
+        if (read_be32(currentPtr + 0 * 4) != 0x626C7A1AUL /* "blz\x1A" */
+            || read_be32(currentPtr + 1 * 4) != 1) {
+            #ifdef MODULE_LOGGING
+            print("invalid header in compressed file");
+            #endif
+            goto out;
+        }
+
+        /* Check blocksize is sufficient */
+		if (hdr_packedsize > max_block_size
+		 || hdr_depackedsize > blocksize) {
+            #ifdef MODULE_LOGGING
+			print("compressed file requires a bigger block size");
+			#endif
+            goto out;
+		}
+
+        currentPtr += HEADER_SIZE;
+        depackedsize = blz_depack(currentPtr, data, (unsigned long) hdr_depackedsize);
+        currentPtr += hdr_packedsize; 
+        
+        if (depackedsize != hdr_depackedsize) {
+            goto out;
+        }
+
+        DWORD written;
+        WriteFile(hFile, (const char*)data, (DWORD)depackedsize, &written, NULL);
+    }
+out:
+    CloseHandle(hFile);
+    HeapFree(handle, 0, data);
+    HeapFree(handle, 0, packed);
+    
+    return;
+}
+
+#endif
