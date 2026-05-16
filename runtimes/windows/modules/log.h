89f9d7ce8e563a053dd2804d02c634c93e24372a
Vulpi <78976654+imvulpi@users.noreply.github.com>
2026-05-17 00:22:46 +0200

    docs: add docs for print in log.h

--- runtimes/windows/modules/log.h
+++ runtimes/windows/modules/log.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #ifdef MODULE_LOGGING
-    extern void print(const char* msg);
+  /// @brief Outputs a simple message to the console.
+  /// @param msg Contents of the message.
+  extern void print(const char* msg);
 #else
   // Macro replacement that is removed from compilation.
   #define print(msg) ((void)0) 
