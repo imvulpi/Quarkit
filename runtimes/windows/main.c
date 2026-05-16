265209a4a2697c817583e303010854b232ba6f00
Vulpi <78976654+imvulpi@users.noreply.github.com>
2026-05-17 00:22:24 +0200

    refactor: remove conditionals for extracts.

--- runtimes/windows/main.c
+++ runtimes/windows/main.c
@@ -1,12 +1,7 @@
 #include <windows.h>
 #include "../shared/hooks.h"
 #include "./modules/log.h"
-
-#ifdef MODULE_EXTRACTION
-/// @brief Extracts the embedded payload to a file.
-/// @param filename Name of the file it should extract to.
-extern void extract(const char* filename);
-#endif
+#include "./modules/extractor/extractor.h"
 
 /// @brief Main windows installer entry. Runs core modules. 
 /// @return 0 if no errors.
@@ -14,10 +9,7 @@ int mainCRTStartup(){
     int stage = INIT;
     
     print("Starting the installation process...\n");
-    
-    #ifdef MODULE_EXTRACTION
-        extract("extracted"); // Temporary name - later configured by cli/user.
-    #endif
+    extract("extracted"); // Temporary name - later configured by cli/user.
 
     ExitProcess(0);
     return 0;
