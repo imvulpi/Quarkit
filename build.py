6c5923bd839b9009d93b6f3701ad4d4c31c8a2fa
Vulpi <78976654+imvulpi@users.noreply.github.com>
2026-05-25 22:18:36 +0200

    feat: add distribution of modules in build system.

--- build.py
+++ build.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import os
+import shutil
 import sys
 from scripts.build_config import BuildConfig
 from scripts.build_enums import Arch, CompilerType, System
 from scripts.cli import cli
+from scripts.globals import DIST_DIR, MODULES_DIR
 from scripts.modules import xz
 
 def main():
@@ -18,9 +20,17 @@ def main():
                BuildConfig(CompilerType.CLANG, System.WINDOWS, Arch.X64, host_system),
                BuildConfig(CompilerType.CLANG, System.WINDOWS, Arch.ARM64, host_system)]
     
+    # First run to generate all the modules once
+    for config in configs:
+        xz.build(config)
+
+    # Build cli for each config and copy the modules
     for config in configs:
         cli.build(config)
-        xz.build(config)    
+        dist_modules = f"{DIST_DIR}/{config.target_system}_{config.target_arch}/modules"
+        shutil.copytree(MODULES_DIR, dist_modules, dirs_exist_ok=True)
 
+    shutil.rmtree(MODULES_DIR)
+    
 if __name__ == "__main__":
     main()
