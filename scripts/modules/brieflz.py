50f1610c49b3bff7659b81b2ecd9ee4d3a6a6ad9
Vulpi <78976654+imvulpi@users.noreply.github.com>
2026-05-30 18:55:46 +0200

    fix: building of brieflz executable.

--- scripts/modules/brieflz.py
+++ scripts/modules/brieflz.py
@@ -7,11 +7,11 @@ from ..globals import MODULES_DIR
 def build(config: BuildConfig):
     print("Building the brieflz module")
     sources = [
-        "modules/deps/brieflz/blzpack.c", 
-        "modules/deps/brieflz/brieflz.c", 
-        "modules/deps/brieflz/parg.c", 
-        "modules/deps/brieflz/depack.c", 
-        "modules/deps/brieflz/depacks.c"
+        "modules/brieflz/deps/blzpack.c", 
+        "modules/brieflz/deps/brieflz.c", 
+        "modules/brieflz/deps/parg.c", 
+        "modules/brieflz/deps/depack.c", 
+        "modules/brieflz/deps/depacks.c"
     ]
         
     output_dir = f"{MODULES_DIR}/brieflz/{config.get_triple()}"
