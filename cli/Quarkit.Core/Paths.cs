1724bcb5318905529e2fef4d77b4da3f44e5dd01
Vulpi <78976654+imvulpi@users.noreply.github.com>
2026-06-01 18:38:47 +0200

    feat: add required Paths for build engine

---
+++ cli/Quarkit.Core/Paths.cs
@@ -0,0 +1,25 @@
+﻿using Quarkit.Models.Manifest;
+
+namespace Quarkit.Core
+{
+    public static class Paths
+    {
+        public static string INSTALLERS_ROOT_DIR = "installer";
+        public static string MODULES_ROOT_DIR = "modules";
+
+        public static string GetModuleDir(string moduleName)
+        {
+            return Path.Join(MODULES_ROOT_DIR, moduleName);
+        }
+
+        public static string GetInstallerDir(OSSystem system)
+        {
+            return Path.Join(INSTALLERS_ROOT_DIR, system.AsString());
+        }
+
+        public static string GetInstallerIncludes(OSSystem system)
+        {
+            return Path.Join(INSTALLERS_ROOT_DIR, system.AsString(), "include");
+        }
+    }
+}
