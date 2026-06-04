e2edaab138cc6bc49159051851a1471c19abfb71
Vulpi <78976654+imvulpi@users.noreply.github.com>
2026-06-04 20:36:11 +0200

    feat: add useful IO defines in windows glue

--- installer/windows/include/windows_glue.h
+++ installer/windows/include/windows_glue.h
@@ -19,6 +19,17 @@ typedef const char* LPCSTR;
 #define FILE_ATTRIBUTE_NORMAL 0x00000080
 #define GENERIC_WRITE 0x40000000
 #define HEAP_ZERO_MEMORY 0x00000008
+#define GENERIC_READ 0x80000000L
+#define FILE_SHARE_READ 0x00000001
+#define OPEN_EXISTING 3
+#define CREATE_ALWAYS 2
+#define FILE_ATTRIBUTE_NORMAL 0x00000080
+#define PAGE_READONLY 0x02
+#define FILE_MAP_READ 0x0004
+#define MAX_PATH 260
+#define FILE_BEGIN 0
+#define FILE_CURRENT 1
+#define FILE_END 2
 
 // Struct for retrieving 64-bit file sizes
 typedef union _LARGE_INTEGER {
