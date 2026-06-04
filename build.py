5534780ae38e0fee4fa486993db5be1aadbb002d
Vulpi <78976654+imvulpi@users.noreply.github.com>
2026-06-04 20:39:55 +0200

    fix: building of modules and copying installer

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ from scripts.build_config import BuildConfig
 from scripts.build_enums import Arch, Bitness, CompilerType, System
 from scripts.cli import cli
 from scripts.globals import DIST_DIR, MODULES_DIR
-from scripts.modules import brieflz, lzma
+from scripts.modules import brieflz, lzma, other
 
 def main():
     print("Initiating Quarkit Building...")
@@ -16,15 +16,18 @@ def main():
     host_system: System = System.UNKNOWN
     if(sys.platform.startswith("win")): host_system = System.WINDOWS
     elif(sys.platform.startswith("linux")): host_system = System.LINUX
-    configs = [BuildConfig(CompilerType.CLANG, System.WINDOWS, Arch.x86, Bitness.x64, host_system),
-               BuildConfig(CompilerType.CLANG, System.WINDOWS, Arch.x86, Bitness.x32, host_system),
-               BuildConfig(CompilerType.CLANG, System.WINDOWS, Arch.Arm, Bitness.x32, host_system),
-               BuildConfig(CompilerType.CLANG, System.WINDOWS, Arch.Arm, Bitness.x64, host_system)]
-    
+    configs = [
+        BuildConfig(CompilerType.CLANG, System.WINDOWS, Arch.x86, Bitness.x64, host_system),
+        BuildConfig(CompilerType.CLANG, System.WINDOWS, Arch.x86, Bitness.x32, host_system),
+        BuildConfig(CompilerType.CLANG, System.WINDOWS, Arch.Arm, Bitness.x32, host_system),
+        BuildConfig(CompilerType.CLANG, System.WINDOWS, Arch.Arm, Bitness.x64, host_system)
+    ]
+
     # First run to generate all the modules once
     for config in configs:
         lzma.build(config)
         brieflz.build(config)
+        other.build(config)
 
     # Build cli for each config and copy the modules
     for config in configs:
@@ -33,6 +36,7 @@ def main():
         shutil.copytree(MODULES_DIR, dist_modules, dirs_exist_ok=True)
 
     shutil.rmtree(MODULES_DIR)
+    shutil.copytree("installer", f"output/{config.get_triple()}/installer")
     
 if __name__ == "__main__":
     main()
---
+++ scripts/modules/other.py
@@ -0,0 +1,15 @@
+import shutil
+
+from ..build_config import BuildConfig
+from ..globals import MODULES_DIR
+
+def build(config: BuildConfig):
+    # Place modules that need to simply be copied.
+    modules = [
+        "quarkit_outputter",
+        "quarkit-extraction"
+    ]
+    
+    for module in modules:
+        output_dir = f"{MODULES_DIR}/{module}"
+        shutil.copytree(f"modules/{module}", f"{output_dir}", dirs_exist_ok=True)
