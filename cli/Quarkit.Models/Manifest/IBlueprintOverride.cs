0defa756d8ee327560d0b049e2761f158501b2db
Vulpi <78976654+imvulpi@users.noreply.github.com>
2026-06-06 01:00:07 +0200

    feat: add interface for overrides of some value

---
+++ cli/Quarkit.Models/Manifest/IBlueprintOverride.cs
@@ -0,0 +1,12 @@
+﻿namespace Quarkit.Models.Manifest;
+public interface IBlueprintOverride<T>
+{
+    public T Value { get; set; }
+    public TargetKey? TargetKey { get; set; }
+    public List<string>? Expressions { get; set; }
+}
+
+public interface IMergeable<T>
+{
+    public void MergeFrom(T source);
+}
