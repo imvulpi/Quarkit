23dc8a496a35eb9338702bd2fadd2e05dcc87d8e
Vulpi <78976654+imvulpi@users.noreply.github.com>
2026-06-25 22:28:37 +0200

    chore: remove unused code

--- cli/Quarkit.Models/Manifest/Modules/ResolvedModule.cs
+++ cli/Quarkit.Models/Manifest/Modules/ResolvedModule.cs
@@ -4,16 +4,5 @@
     {
         public required LoadedModule Module { get; set; }
         public required ModuleBlueprint Blueprint { get; set; }
-
-        /// <summary>
-        /// Returns absolute file paths to all C sources so the BuildEngine can read them directly.
-        /// </summary>
-        public IEnumerable<string> GetAbsoluteCSources()
-        {
-            if (Blueprint.CSources == null || Blueprint.CSources.Values == null) return [];
-
-            return Blueprint.CSources.Values.Select(src =>
-                Path.GetFullPath(Path.Combine(Module.ModuleDirectory, src)));
-        }
     }
 }
